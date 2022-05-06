dde61ce733669bbf53d00af173006e10e6bbd007
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-05-06 17:30:14 -0600

    smoke test

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,8 +1,20 @@
 package com.skilldistillery.film.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.skilldistillery.film.dao.FilmDAO;
 
 @Controller
 public class FilmController {
+	
+	@Autowired
+	private FilmDAO filmDao;
+	
+	@RequestMapping(path = {"/", "home.do"})
+	public String home() {
+		return "WEB-INF/views/home.jsp";
+	}
 
 }
---
+++ src/main/java/com/skilldistillery/film/dao/FilmDaoJdbcImpl.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.film.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FilmDaoJdbcImpl implements FilmDAO {
+	
+	
+	
+	
+}
