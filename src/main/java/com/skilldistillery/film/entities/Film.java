8e9673f322c79b3fd721fa2500a147f51f6ca35c
paul-chaffin <pechaffin@pm.me>
2022-05-06 17:33:40 -0400

    create .gitignore

---
+++ src/main/java/com/skilldistillery/film/entities/Film.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.film.entities;
+
+public class Film {
+
+}
---
+++ src/test/java/com/skilldistillery/film/entities/FilmTest.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.film.entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FilmTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
